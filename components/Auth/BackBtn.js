6ba3aa2f59e2d31cfe1ee926b8eea12b7040df64
himzei <himzei@gmail.com>
2021-03-05 18:49:55 +0900

    8.2 welcome screen part one

---
+++ components/Auth/BackBtn.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Platform } from "react-native";
+import styled from "styled-components/native";
+
+
+const isAndroid = Platform.OS === "android";
+
+const Container = styled.View`
+    padding-left: 20px;
+`;
+
+
+export default () => (
+  <Container>
+    <Ionicons name={isAndroid ? "md-arrow-down" : "ios-arrow-down"} size={28} />
+  </Container>
+);
--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,14 +1,41 @@
+import { StatusBar } from "expo-status-bar";
 import React from "react";
+import styled from "styled-components/native";
+import { BlurView } from "expo-blur";
 
-import { View, Text, Button } from "react-native";
+const Container = styled.View`
+  flex: 1;
+`;
+
+const Image = styled.Image`
+  position: absolute;
+  z-index: -1;
+  top: 0;
+`;
+
+const Logo = styled.Image`
+  width: 150px;
+  height: 150px;
+`;
 
 export default ({ navigation }) => {
   console.log(navigation);
   return (
-    <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
-      <Text>Welcome</Text>
-      <Button onPress={() => navigation.navigate("SignUp")} title={"Sign Up"} />
-      <Button onPress={() => navigation.navigate("SignIn")} title={"Sign In"} />
-    </View>
+    <Container>
+      <BlurView
+        tint="light"
+        intensity={70}
+        style={{
+          flex: 1,
+          width: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Logo source={require("../assets/logo2.png")} />
+      </BlurView>
+      <Image source={require("../assets/loginBg.jpeg")} />
+      <StatusBar barStyle="light-content" />
+    </Container>
   );
 };
