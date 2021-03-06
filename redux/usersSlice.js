a9af48af8e57c5e8494b8f59e03720d8b806b619
himzei <himzei@gmail.com>
2021-03-06 16:26:16 +0900

    10.0 getting favourite

--- redux/usersSlice.js
+++ redux/usersSlice.js
@@ -11,6 +11,7 @@ const userSlice = createSlice({
     logIn(state, action) {
       state.isLoggedIn = true;
       state.token = action.payload.token;
+      state.id = action.payload.id; 
     },
     logOut(state, action) {
       state.isLoggedIn = false;
@@ -25,12 +26,21 @@ export const userLogin = (form) => async dispatch => {
   try{
     const {data: {id, token}} = await api.login(form); 
     if(id && token){
-      dispatch(logIn({token}))
+      dispatch(logIn({token, id}))
     }
   }catch(e){
     alert("Wrong user/password")
   }
 }
 
+export const getFavs = () => async (dispatch, getState) => {
+  const { usersReducer: {id} } = getState();
+  try{
+    const {data} = await api.favs(id);
+    console.log(data);
+  }catch(e){
+    console.warn(e)
+  }
+}
 
 export default userSlice.reducer;
