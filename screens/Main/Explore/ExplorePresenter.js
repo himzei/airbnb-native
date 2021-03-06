4ee1c5deccf7b38039ef0a5a6b4f7ecc851bb49d
himzei <himzei@gmail.com>
2021-03-06 12:43:35 +0900

    9.5 roomcard_1

--- screens/Main/Explore/ExplorePresenter.js
+++ screens/Main/Explore/ExplorePresenter.js
@@ -1,16 +1,34 @@
 import React from "react";
+import { ActivityIndicator } from "react-native";
 import styled from "styled-components/native";
+import RoomCard from "../../../components/RoomCard";
 
 const Container = styled.View`
-    flex: 1;
-    justify-content: center;
-    align-items: center;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Text = styled.Text``;
 
-export default () => (
-  <Container>
-    <Text>Explorer</Text>
-  </Container>
-);
+export default ({ rooms }) => {
+  return (
+    <Container>
+      {rooms.length === 0 ? (
+        <ActivityIndicator color="black" />
+      ) : (
+        rooms.map((room) => (
+          <RoomCard
+            price={room.price}
+            key={room.id}
+            name={room.name}
+            id={room.id}
+            isFav={room.is_fav}
+            photos={room.photos}
+            isSuperHost={room.user.superhost}
+          />
+        ))
+      )}
+    </Container>
+  );
+};
