d034b1a126e627a4f6fb19ebf2d9aac0b05c2e32
himzei <himzei@gmail.com>
2021-03-06 12:20:14 +0900

    duplicate bugfix

--- redux/roomsSlice.js
+++ redux/roomsSlice.js
@@ -1,22 +1,46 @@
 import { createSlice } from "@reduxjs/toolkit";
+import api from "../api";
 
 const roomsSlice = createSlice({
   name: "rooms",
   initialState: {
     explore: {
-        page: 1,
-        rooms: []
+      page: 1,
+      rooms: [],
     },
     favs: [],
   },
   reducers: {
     setExploreRooms(state, action) {
-      state.explore.rooms.push(action.payload.rooms);
-      state.explore.page = action.payload.page;
+      const { explore } = state;
+      const { payload } = action;
+      payload.rooms.forEach((payloadRoom) => {
+        const exists = explore.rooms.find(
+          (savedRoom) => savedRoom.id === payloadRoom.id
+        );
+        if (!exists) {
+          explore.rooms.push(payloadRoom);
+        }
+      });
+      state.explore.page = payload.page;
     },
   },
 });
 
-const {setExploreRooms} = roomsSlice.actions; 
+const { setExploreRooms } = roomsSlice.actions;
 
-export default roomsSlice.reducer
+export const getRooms = () => async (dispatch) => {
+  try {
+    const {
+      data: { results },
+    } = await api.rooms();
+    dispatch(
+      setExploreRooms({
+        rooms: results,
+        page: 1,
+      })
+    );
+  } catch (e) {}
+};
+
+export default roomsSlice.reducer;
--- screens/Auth/SignIn/SignInContainer.js
+++ screens/Auth/SignIn/SignInContainer.js
@@ -7,8 +7,8 @@ import utils from "../../../utils";
 
 export default ({ route: { params } }) => {
   const dispatch = useDispatch();
-  const [email, setEmail] = useState(params?.email);
-  const [password, setPassword] = useState(params?.password);
+  const [email, setEmail] = useState(params?.email || "himzei@hotmail.com");
+  const [password, setPassword] = useState(params?.password || "1111");
   const isFormValid = () => {
     if (email === "" || password === "") {
       alert("All Fields are required!!!");
